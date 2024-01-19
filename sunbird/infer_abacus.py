e004678ade73f07b3af908d0885dfa293023df22
epaillas <enrique.paillas@waterloo.ca>
2024-01-19 07:53:00 -0800

    combined inference working

--- sunbird/infer_abacus.py
+++ sunbird/infer_abacus.py
@@ -2,13 +2,13 @@ import argparse
 import yaml
 import time
 from pathlib import Path
-from sunbird.inference import HMC
+from sunbird.inference import HMC, Nested
 
 if __name__ == "__main__":
-    output_path = Path("/pscratch/sd/e/epaillas/sunbird/chains/voxel_voids_fullap/")
+    output_path = Path("/pscratch/sd/e/epaillas/sunbird/chains/dsl/")
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--config_path", type=str, default="infer_voxel_abacus.yaml"
+        "--config_path", type=str, default="infer_abacus.yaml"
     )
     # Make sure it reads from dataset with fixed hod
     parser.add_argument("--cosmology", type=int, default=0)
@@ -18,22 +18,20 @@ if __name__ == "__main__":
         config = yaml.safe_load(f)
     config["data"]["observation"]["get_obs_args"]["cosmology"] = args.cosmology
     loss = config["theory_model"]["args"]["loss"]
-    vol = config["data"]["covariance"]["volume_scaling"]
-    smin = config["slice_filters"]["s"][0]
-    smax = config["slice_filters"]["s"][1]
+    # smin = config["slice_filters"]["s"][0]
+    # smax = config["slice_filters"]["s"][1]
     statistics = "_".join([i for i in config["statistics"]])
-    multipoles = ''.join([str(i) for i in config["select_filters"]["multipoles"]])
-    dir_store = f"abacus_c{args.cosmology}_{statistics}_"\
-                f"{loss}_abacuscov_vol{vol}_smin{smin:.2f}_smax{smax:.2f}_m{multipoles}"
+    # multipoles = ''.join([str(i) for i in config["select_filters"]["multipoles"]])
+    dir_store = f"abacus_c{args.cosmology}_{statistics}"
     if args.suffix is not None:
         dir_store += f"_{args.suffix}"
     config["inference"]["output_dir"] = output_path / dir_store
     print("output dir")
     print(config["inference"]["output_dir"])
-    hmc = HMC.from_config_dict(
+    nested = Nested.from_config_dict(
         config=config,
     )
     t0 = time.time()
-    print(f"Fitting parameters {hmc.param_names}")
-    hmc()
+    print(f"Fitting parameters {nested.param_names}")
+    nested()
     print("Fitting took = ", time.time() - t0)
